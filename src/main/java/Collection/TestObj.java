f9fd5da70deae9c90c03bb75cc3dee5ad47a3638
pyc's work computer <piyicheng001@qq.com>
2019-12-20 17:00:45 +0800

    company computer

---
+++ src/main/java/Collection/TestObj.java
@@ -0,0 +1,17 @@
+package Collection;
+
+public class TestObj {
+    private String valueA;
+
+    public String getValueA() {
+        return valueA;
+    }
+
+    public void setValueA(String valueA) {
+        this.valueA = valueA;
+    }
+
+    public TestObj(String valueA) {
+        this.valueA = valueA;
+    }
+}
