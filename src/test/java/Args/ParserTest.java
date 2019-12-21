fdb3303762b2caa3af4105f8cd7f88d15b5c57c6
pi <piyicheng001@qq.com>
2019-12-21 14:19:36 +0800

    linux

--- src/test/java/Args/ParserTest.java
+++ src/test/java/Args/ParserTest.java
@@ -7,36 +7,36 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ParserTest {
-
-    private Args args;
-    private Parser parser;
-    private Schema schema;
-
-    @Before
-    public void initial_object() {
-        args = new Args();
-        parser = new Parser();
-        schema = new Schema();
-    }
-
-    @Test
-    public void parse_command_line(){
-        Parser parser = new Parser("-l -p 8080 -d /usr/local");
-        assertThat(true,is(parser.getCommandValue("l")));
-
-    }
-
-    @Test
-    public void return_boolean_when_flag_is_l() {
-
-        assertThat(true,is(parser.isCorrectType("bealean", schema)));
-        assertThat(false,is(parser.isCorrectType("integer",schema)));
-    }
-
-    @Test
-    public void return_integer_when_flag_is_p(){
-        assertThat(true,is(parser.isCorrectType("integer",schema)));
-    }
+//
+//    private Args args;
+//    private Parser parser;
+//    private Schema schema;
+//
+//    @Before
+//    public void initial_object() {
+//        args = new Args();
+//        parser = new Parser();
+//        schema = new Schema();
+//    }
+//
+//    @Test
+//    public void parse_command_line(){
+//        Parser parser = new Parser("-l -p 8080 -d /usr/local");
+//        assertThat(true,is(parser.getCommandValue("l")));
+//
+//    }
+//
+//    @Test
+//    public void return_boolean_when_flag_is_l() {
+//
+//        assertThat(true,is(parser.isCorrectType("bealean", schema)));
+//        assertThat(false,is(parser.isCorrectType("integer",schema)));
+//    }
+//
+//    @Test
+//    public void return_integer_when_flag_is_p(){
+//        assertThat(true,is(parser.isCorrectType("integer",schema)));
+//    }
 
 
 }
